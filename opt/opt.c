e747680a66d427fc849aa399e04159f9729aaa26
Lurgrid <Lurgrid@gmail.com>
2023-03-10 15:36:52 +0100

    fix

--- opt/opt.c
+++ opt/opt.c
@@ -5,6 +5,10 @@
 #define SHORT_HELP "-h"
 #define LONG_HELP "--help"
 
+#define LONG(opt) ((opt)->optlong)
+#define SHORT(opt) ((opt)->optshort)
+#define ARG(opt) ((opt)->arg)
+
 struct optparam {
   const char *optshort;
   const char *optlong;
@@ -38,7 +42,8 @@ static const char *prefix(const char *s1, const char *s2) {
   return prefix(s1 + 1, s2 + 1);
 }
 
-static int opt_parse(const char *optlong, const char *optshort, bool arg, int *k,
+static int opt_parse(const char *optlong, const char *optshort, bool arg,
+    int *k,
     char **argv, int argc, const char **option) {
   if (strcmp(optshort, argv[*k]) == 0) {
     if (arg) {
@@ -46,47 +51,54 @@ static int opt_parse(const char *optlong, const char *optshort, bool arg, int *k
         *option = argv[*k];
         return -1;
       }
-      *option = argv[*k + 1];
       ++(*k);
-      return 0;
     }
     *option = argv[*k];
     return 0;
   }
-  const char *p = prefix(optlong, argv[*k]);
-  if (p == NULL) {
-    *option = NULL;
-    return -1;
+  if (arg) {
+    const char *p = prefix(optlong, argv[*k]);
+    if (p == NULL) {
+      *option = NULL;
+      return -1;
+    }
+    if (*p != '=') {
+      *option = argv[*k];
+      return -1;
+    }
+    *option = p + 1;
+    return 0;
   }
-  if (*p != '=') {
+  if (strcmp(optlong, argv[*k]) == 0) {
     *option = argv[*k];
-    return -1;
+    return 0;
   }
-  *option = p + 1;
-  return 0;
+  *option = NULL;
+  return -1;
 }
+
+#define PRINT_OPTION(opt) printf("\t%s%s | %s%s : %s\n", opt->optshort,        \
+    (opt->arg ? " [option]" : ""), opt->optlong,                               \
+    (opt->arg ? "=[option]" : ""), opt->desc)                                  \
 
 optreturn opt_init(char **argv, int argc, optparam **aopt,
     size_t nmemb, void (*other)(void *cntxt, const char *value), void *cntxt) {
   for (int k = 0; k < argc; ++k) {
     if (strcmp(SHORT_HELP, argv[k]) == 0 || strcmp(LONG_HELP, argv[k]) == 0) {
       for (size_t i = 0; i < nmemb; ++i) {
-        printf("\t%s%s | %s%s : %s\n", aopt[i]->optshort,
-            (aopt[i]->arg ? " [option]" : ""), aopt[i]->optlong,
-            (aopt[i]->arg ? "=[option]" : ""), aopt[i]->desc);
+        PRINT_OPTION(aopt[i]);
       }
       return STOP_PROCESS;
     }
     size_t i = 0;
     while (i < nmemb) {
       const char *v;
-      if (opt_parse(aopt[i]->optlong, aopt[i]->optshort, aopt[i]->arg, &k, argv,
-          argc, &v) != 0) {
-        if (v != NULL) {
+      int r = opt_parse(LONG(aopt[i]), SHORT(aopt[i]), ARG(aopt[i]), &k, argv,
+          argc, &v);
+      if (v != NULL) {
+        if (r != 0) {
           return ERROR_PARAM;
         }
-      }
-      if (v != NULL) {
         if (aopt[i]->fun(cntxt, v) != 0) {
           return ERROR_FUN;
         }
