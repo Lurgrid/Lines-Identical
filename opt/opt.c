35d8f26d48239c24e6939f8306418cc226e4eda5
Lurgrid <Lurgrid@gmail.com>
2023-03-10 18:04:06 +0100

    commentary & implementation of opt

--- opt/opt.c
+++ opt/opt.c
@@ -1,14 +1,18 @@
+// opt.c : partie implantation de l'interface opt.h
+
 #include "opt.h"
+
 #include <string.h>
 #include <stdio.h>
 
-#define SHORT_HELP "-h"
-#define LONG_HELP "--help"
+//--- Définition optparam ------------------------------------------------------
 
-#define LONG(opt) ((opt)->optlong)
-#define SHORT(opt) ((opt)->optshort)
-#define ARG(opt) ((opt)->arg)
-
+//  struct optparam, optparam : representation d'une option d'un programme, les
+//    champs optshort, optlong représente respectivement les version courte et
+//    longue de l'option. desc est une chaine de carractère, la description de
+//    l'option. le booléen vaut vrai si l'option demande un paramètre et faux
+//    sinon. la fonction fun représente les actions l'ors de l'appel de l'option
+//    .
 struct optparam {
   const char *optshort;
   const char *optlong;
@@ -16,6 +20,27 @@ struct optparam {
   bool arg;
   int (*fun)(void *cntxt, const char *value);
 };
+
+//--- Racourcis ----------------------------------------------------------------
+
+#define LONG(opt) ((opt)->optlong)
+#define SHORT(opt) ((opt)->optshort)
+#define ARG(opt) ((opt)->arg)
+
+//--- Divers -------------------------------------------------------------------
+
+//  prefix : Renvoie NULL si s1 n'est pas prefix de s2, si s1 est egale a s2
+//    alors renvois le pointeur du carractère nul de s2 sinon renvois le
+//    pointeur sur le premier carractère de s2 qui n'appartient pas a s1.
+static const char *prefix(const char *s1, const char *s2) {
+  if (*s1 == '\0') {
+    return s2;
+  }
+  if (*s2 == '\0' || *s1 != *s2) {
+    return NULL;
+  }
+  return prefix(s1 + 1, s2 + 1);
+}
 
 optparam *opt_gen(const char *optshort, const char *optlong, const char *desc,
     bool arg, int (*fun)(void *cntxt, const char *value)) {
@@ -31,70 +56,82 @@ optparam *opt_gen(const char *optshort, const char *optlong, const char *desc,
   return op;
 }
 
-//  prefix : Renvoie le premier caractère de s2 qui
-static const char *prefix(const char *s1, const char *s2) {
-  if (*s1 == '\0') {
-    return s2;
-  }
-  if (*s2 == '\0' || *s1 != *s2) {
-    return NULL;
-  }
-  return prefix(s1 + 1, s2 + 1);
-}
+//  parse_return : Spécifie les valeurs de retour de la fonction opt_check
+enum parse_return {
+  SUCCESS_PARAM,
+  FAILURE_PARAM,
+  NOT_EQUAL,
+};
 
-static int opt_parse(const char *optlong, const char *optshort, bool arg,
-    int *k,
-    char **argv, int argc, const char **option) {
-  if (strcmp(optshort, argv[*k]) == 0) {
-    if (arg) {
-      if (*k + 1 >= argc) {
-        *option = argv[*k];
-        return -1;
+//  opt_check : Vérifie si la chaine d'indice k du tableau argv (de longueur
+//    argc) correspond à une entrée correct de l'option pointer par opt.
+//    Retourne SUCCESS_PARAM si l'entrée était correct, option pointe alors sur
+//    argv[k + 1] si le l'option a besoin d'un paramètre sinon pointe sur
+//    argv[k]. Si la valuer retourner est egale a NOT_EQUAL alors l'entrée n'est
+//    pas l'option pointer par opt et option vaut NULL. Enfin FAILURE_PARAM est
+//    renvoyer quand l'utilisateur a rentrée la bonne option mais a oublier le
+//    paramètre.
+static int opt_parse(const optparam *opt, int k, char **argv, int argc,
+    const char **option) {
+  if (strcmp(SHORT(opt), argv[k]) == 0) {
+    if (ARG(opt)) {
+      if (k + 1 >= argc) {
+        *option = argv[k];
+        return FAILURE_PARAM;
       }
-      ++(*k);
+      ++k;
     }
-    *option = argv[*k];
-    return 0;
+    *option = argv[k];
+    return SUCCESS_PARAM;
   }
-  if (arg) {
-    const char *p = prefix(optlong, argv[*k]);
+  if (ARG(opt)) {
+    const char *p = prefix(LONG(opt), argv[k]);
     if (p == NULL) {
       *option = NULL;
-      return -1;
+      return NOT_EQUAL;
     }
     if (*p != '=') {
-      *option = argv[*k];
-      return -1;
+      *option = argv[k];
+      return FAILURE_PARAM;
     }
     *option = p + 1;
-    return 0;
+    return SUCCESS_PARAM;
   }
-  if (strcmp(optlong, argv[*k]) == 0) {
-    *option = argv[*k];
+  if (strcmp(LONG(opt), argv[k]) == 0) {
+    *option = argv[k];
     return 0;
   }
   *option = NULL;
-  return -1;
+  return NOT_EQUAL;
 }
 
 #define PRINT_OPTION(opt) printf("\t%s%s | %s%s : %s\n", opt->optshort,        \
     (opt->arg ? " [option]" : ""), opt->optlong,                               \
     (opt->arg ? "=[option]" : ""), opt->desc)                                  \
 
+#define SHORT_HELP "-h"
+#define LONG_HELP "--help"
+
 optreturn opt_init(char **argv, int argc, optparam **aopt,
-    size_t nmemb, void (*other)(void *cntxt, const char *value), void *cntxt) {
+    size_t nmemb, void (*other)(void *cntxt, const char *value), void *cntxt,
+    const char *usage, const char* desc) {
   for (int k = 0; k < argc; ++k) {
     if (strcmp(SHORT_HELP, argv[k]) == 0 || strcmp(LONG_HELP, argv[k]) == 0) {
+      if (usage != NULL) {
+        printf("Usage:%s\n\n", usage);
+      }
       for (size_t i = 0; i < nmemb; ++i) {
         PRINT_OPTION(aopt[i]);
+      }
+      if (desc != NULL) {
+        printf("\n%s", desc);
       }
       return STOP_PROCESS;
     }
     size_t i = 0;
     while (i < nmemb) {
       const char *v;
-      int r = opt_parse(LONG(aopt[i]), SHORT(aopt[i]), ARG(aopt[i]), &k, argv,
-          argc, &v);
+      int r = opt_parse(aopt[i], k, argv, argc, &v);
       if (v != NULL) {
         if (r != 0) {
           return ERROR_PARAM;
@@ -102,6 +139,7 @@ optreturn opt_init(char **argv, int argc, optparam **aopt,
         if (aopt[i]->fun(cntxt, v) != 0) {
           return ERROR_FUN;
         }
+        ++k;
         break;
       }
       ++i;
--- opt/opt.h
+++ opt/opt.h
@@ -1,7 +1,7 @@
 //  Partie interface du module opt (gestion d'option).
 //
-//  Ce module permet de mettre en place une gestion d'option pour son
-//    utilisateur.
+//  Ce module permet de mettre en place au sein d'un programme, une gestion
+//    d'options pour son utilisateur.
 
 #ifndef OPT__H
 #define OPT__H
@@ -9,8 +9,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+//  struct optparam optparam : type et nom de type d'un contrôleur regroupant
+//    les informations nécessaire pour gérer une option.
 typedef struct optparam optparam;
 
+//  optreturn : enumaration des valeurs de retours possible de la fonctions
+//    opt_init. (chaque utilisation des valeurs des enumes est spécifier dans la
+//    spécification de la fonction opt_init)
 typedef enum {
   STOP_PROCESS,
   ERROR_FUN,
@@ -18,9 +23,32 @@ typedef enum {
   DONE,
 } optreturn;
 
+//  opt_gen : Tente d'allouer les resources néssaires pour gérer une options.
+//    Avec valeur initiale les champs optshort, optlong représente
+//    respectivement les version courte et longue de l'option. desc est une
+//    chaine de carractère, la description de l'option. le booléen vaut vrai si
+//    l'option demande un paramètre et faux sinon. la fonction fun représente
+//    les actions l'ors de l'appel de l'option. Renvoie NULL en cas de
+//    dépassement de capacité. Renvoie sinon un pointer vers la structure
+//    associé a cette option.
 extern optparam *opt_gen(const char *optshort, const char *optlong,
     const char *desc, bool arg, int (*fun)(void *cntxt, const char *value));
 
+//  opt_init : Pour toutes les options représenté dans le tableau argv de
+//    longueur argc, qui sont présente dans le talbeau d'option aopt, effectue
+//    effectue le traitement adequatement definie a la création de l'option
+//    (avec comme paramettre a ce traitement cntxt et si l'option necessite un
+//    argument alors passe la chaine suivante en argument, sinon la chaine
+//    représentant l'option). Si ce traitement echoue par manque d'un paramettre
+//    à l'option actuellement executer, alors la fonction retourne ERROR_PARAM.
+//    Si le traitement de l'option echoue alors, la fonction retourne ERROR_FUN.
+//    Sinon en cas de succés du traitement de toutes les options, retourne DONE.
+//    Une options par default existe, celle du "help", si cette options est
+//    appeler la fonction retourne STOP_PROCESS et n'a donc pas effectuer le
+//    traitement des options pas encore traiter. Pour toutes chaine de caractère
+//    présente dans argv qui ne correspont ni à une options ni a l'arguments
+//    d'une options, le traitement de cette chaine est assurer par la fonction
+//    other qui prend en paramettre cntxt (un pointeur sur un context).
 extern optreturn opt_init(char **argv, int argc, optparam **aopt,
     size_t nmemb, void (*other)(void *cntxt, const char *value), void *cntxt);
 
