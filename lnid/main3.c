6498f25e85d559d541037a064c8fae2d6d8e17c8
Lurgrid <Lurgrid@gmail.com>
2023-05-02 00:00:42 +0200

    .

--- lnid/main3.c
+++ lnid/main3.c
@@ -3,8 +3,8 @@
 #include <time.h>
 #include <string.h>
 
-#define MAX 20000
-#define LEN 80000
+#define MAX 50000000
+#define LEN 80
 
 int main(void) {
   char alp[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
