a01b2d3a539c9dc188ef5744d052e4eeab62f2b4
Lurgrid <Lurgrid@gmail.com>
2023-05-02 21:46:38 +0200

    .
    .

--- lnid/main2.c
+++ lnid/main2.c
@@ -3,36 +3,19 @@
 #include <time.h>
 #include <string.h>
 
-#define MAX 2000
-#define LEN 20000
-
-static size_t str_hashfun(const char *s);
-static size_t np2(size_t n);
+#define MAX 500000
+#define LEN 8000
 
 int main(void) {
   char alp[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
       ",;:!?./*-+@&\"'(-_)=+<> ";
   size_t alp_len = strlen(alp);
-  char ss[MAX][LEN + 1];
-  size_t n = np2(MAX);
   srand((unsigned int) time(NULL));
-  for (size_t i = 0; i < MAX; ++i) {
-    for (size_t j = 0; j < LEN; ++j) {
-      ss[i][j] = alp[i % alp_len];
+  for (long int k = 0; k < MAX; ++k) {
+    for (long int j = 0; j < LEN; ++j) {
+      putchar(alp[(size_t) ((float) rand() / (float) RAND_MAX * (float) alp_len)]);
     }
-    ss[i][LEN] = '\0';
-  }
-  for (size_t i = 0; i < MAX; ++i) {
-    printf("%zu\n", str_hashfun(ss[i]) % n);
-  }
-  size_t hash_val = str_hashfun(ss[0]);
-  for (size_t i = 0; i < MAX; ++i) {
-    if (str_hashfun(ss[i]) != hash_val) {
-      fprintf(stderr,
-          "*** Error: La collision de hachage n'a pas été trouvée\n%s\n",
-          ss[i]);
-      return EXIT_FAILURE;
-    }
+    putchar('\n');
   }
   return EXIT_SUCCESS;
 }
